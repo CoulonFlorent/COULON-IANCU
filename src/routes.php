9dd71f30b2ac6b3e4a0cc4b196bbc39df3017c32
raph <rififi3@hotmail.fr>
2016-09-13 14:48:53 +0200

    Page de Connexion et enregistrement

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,19 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/signin/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'connexion.phtml', $args);
+});
+
+$app->get('/signup/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'enregistrement.phtml', $args);
+});
--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,17 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
+		'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'newbie',
+            'username' => 'root',
+            'password' => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+		
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
